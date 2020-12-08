48122f3271229490df10b0bd6685ce56c432f0fc
unknown <arnavb.co18@nsut.ac.in>
2020-12-08 15:30:53 +0530

    'init'

---
+++ fl.py
@@ -0,0 +1,16 @@
+from flask import Flask, Response, request
+from flask import render_template, make_response
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+api2 = Api(app)
+class Display(Resource):
+	def get(self, string1):
+		headers = {'Content-Type': 'text/html'}
+		return{'text':'test'}
+		# return make_response(render_template("result.html",result= "{}".format(string1)))
+api.add_resource(Display, '/search/v1/<string:string1>')
+
+if __name__ == '__main__':
+	app.run()
